802e9965ce9f10da68113d9b8b4c2194eb3928de
Tdmaxer <ytdilshan07@gmail.com>
2025-04-29 18:58:54 +0530

    Update Projects.jsx

--- road-pets-frontend/src/components/Projects.jsx
+++ road-pets-frontend/src/components/Projects.jsx
@@ -42,47 +42,55 @@ const Projects = (props) => {
         deviceType={props.deviceType}
         dotListClass="custom-dot-list-style"
         itemClass="carousel-item-padding-40-px"
-        
+
       >
-        <div className="carousel-item-wrapper">
+        <div className="carousel-item-wrapper card p-3 text-center h-100 ">
           <img
             src="https://i.ibb.co/FkbsJnLv/77fe0096-e2c4-4b76-acca-a681c755acb2-66b356ad77c48.jpg"
             alt="Description 1"
-            className="img-fluid"
+             className="card-img-top img-fluid mb-3"
+            style={{ maxHeight: '200px', objectFit: 'cover' }}
           />
-          <h3 className="mt-2">Street Pet Rescue & First Aid Camp</h3>
-          <p>Rain volunteers to safely rescue road pets and provide basic first aid.</p>
-          <button className="btn btn-primary">Learn More</button>
+          <h3 className="card-body d-flex flex-column justify-content-between">Street Pet Rescue & First Aid Camp</h3>
+          <p className="card-text">Rain volunteers to safely rescue road pets and provide basic first aid.</p>
+          <button className="btn btn-primary mt-auto">Learn More</button>
         </div>
-        <div className="carousel-item-wrapper">
+
+        <div className="carousel-item-wrapper card p-3 text-center h-100">
           <img
             src="https://i.ibb.co/0RGc0zXs/csm-Taipan-Pacci-Pacca-2015-06-1.png"
             alt="Description 2"
-            className="img-fluid"
+            className="card-img-top img-fluid mb-3"
+            style={{ maxHeight: '200px', objectFit: 'cover' }}
           />
-          <h3 className="mt-2">Feeding & Rescue Patrol</h3>
-          <p>Organize teams to feed and identify sick/injured strays</p>
-          <button className="btn btn-primary">Learn More</button>
+          
+            <h3 className="card-body d-flex flex-column justify-content-between">Feeding & Rescue Mission Campaign</h3>
+            <p className="card-text">Organize teams to feed and identify sick/injured strays</p>
+            <button className="btn btn-primary mt-auto">Learn More</button>
+          
         </div>
-        <div className="carousel-item-wrapper">
+
+        <div className="carousel-item-wrapper card p-3 text-center h-100">
           <img
             src="https://i.ibb.co/YFCrdPQB/dogs-for-vaccination.png"
             alt="Description 3"
-            className="img-fluid"
+             className="card-img-top img-fluid mb-3"
+            style={{ maxHeight: '200px', objectFit: 'cover' }}
           />
-          <h3 className="mt-2">Vet Check-Up & Vaccination Camp</h3>
-          <p>Collaborate with veterinarians to provide free check-ups,and sterilization for street pets.</p>
-          <button className="btn btn-primary">Learn More</button>
+          <h3 className="card-body d-flex flex-column justify-content-between">Vet Check-Up & Vaccination Camp</h3>
+          <p className="card-text">Collaborate with veterinarians to provide free check-ups,and sterilization for street pets.</p>
+          <button className="btn btn-primary mt-auto">Learn More</button>
         </div>
-        <div className="carousel-item-wrapper">
+        <div className="carousel-item-wrapper card p-3 text-center h-100">
           <img
             src="https://i.ibb.co/SwDDyYVJ/Milchzeit-2016-02-16-2.png"
             alt="Description 4"
-            className="img-fluid"
+             className="card-img-top img-fluid mb-3"
+            style={{ maxHeight: '200px', objectFit: 'cover' }}
           />
-          <h3 className="mt-2">"Rescue a Paw" School & College Campaign</h3>
-          <p>Host awareness sessions in schools and colleges to encourage youth involvement.</p>
-          <button className="btn btn-primary">Learn More</button>
+          <h3 className="card-body d-flex flex-column justify-content-between">"Rescue a Paw" School & College Campaign</h3>
+          <p className="card-text">Host awareness sessions in schools and colleges to encourage youth involvement.</p>
+          <button className="btn btn-primary mt-auto">Learn More</button>
         </div>
       </Carousel>
 
