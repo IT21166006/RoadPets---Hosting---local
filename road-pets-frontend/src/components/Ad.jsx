65e7c891697db1419634d7c006b5acd7e7f974ed
Tdmaxer <ytdilshan07@gmail.com>
2025-05-02 13:10:38 +0530

    Update Ad.jsx

--- road-pets-frontend/src/components/Ad.jsx
+++ road-pets-frontend/src/components/Ad.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import myAdImage from '.././../public/petad.png'; 
 
 const Ad = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center ">
+       <Link to="/charityreg">
       <img
         src={myAdImage}
         alt="Advertisement"
         className="w-full h-auto object-contain"
       />
+      </Link>
       
     </div>
   );
